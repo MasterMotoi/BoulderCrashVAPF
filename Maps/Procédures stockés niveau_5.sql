18f76b71892ccc736671fb29dd65c50f3885c870
ADESWAEME <alexandra.deswaeme@viacesi.fr>
2017-06-21 16:36:09 +0200

    Add files via upload

---
+++ Maps/Procédures stockés niveau_5.sql
@@ -0,0 +1,370 @@
+##---PROCEDURE 'findAllniveau_5'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findAllniveau_5`()
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findniveau_5Bycolone_1'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_1`(IN `colone_1` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_1 = colone_1$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_2'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_2`(IN `colone_2` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_2 = colone_2$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_3'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_3`(IN `colone_3` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_3 = colone_3$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_4'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_4`(IN `colon_4` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_4 = colone_4$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_5'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_5`(IN `colone_5` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_5 = colone_5$$
+DELIMITER ;
+
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_6'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_6`(IN `colone_6` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_6 = colone_6$$
+DELIMITER ;
+
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_7'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_7`(IN `colone_7` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_7 = colone_7$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_8'---##
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_8`(IN `colone_8` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_8 = colone_8$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_9'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_9`(IN `colone_9` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_9 = colone_9$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_10'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_10`(IN `colone_10` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_10 = colone_10$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_11'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_11`(IN `colone_11` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_11 = colone_11$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_12'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_12`(IN `colone_12` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_12 = colone_12$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_13'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_13`(IN `colone_13` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_13 = colone_13$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_14'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_14`(IN `colone_14` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_14 = colone_14$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_15'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_15`(IN `colone_15` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_15 = colone_15$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_16'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_16`(IN `colone_16` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_16 = colone_16$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_17'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_17`(IN `colone_17` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_17 = colone_17$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_18'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_18`(IN `colone_18` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_18 = colone_18$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_19'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_19`(IN `colone_19` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_19 = colone_19$$
+DELIMITER ;
+
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_20'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_20`(IN `colone_20` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_20 = colone_20$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_21'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_21`(IN `colone_21` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_21 = colone_21$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_22'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_22`(IN `colone_22` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_22 = colone_22$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_23'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_23`(IN `colone_23` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_23 = colone_23$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_24'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_24`(IN `colone_24` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_24 = colone_24$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_25'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_25`(IN `colone_25` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_25 = colone_25$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_26'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_26`(IN `colone_26` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_26 = colone_26$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_27'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_27`(IN `colone_27` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_27 = colone_27$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_28'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_28`(IN `colone_28` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_28 = colone_28$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_29'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_29`(IN `colone_29` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_29 = colone_29$$
+DELIMITER ;
+
+
+
+##---PROCEDURE 'findAllniveau_5Bycolone_30'---##
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findniveau_5Bycolone_30`(IN `colone_30` INT)
+    NO SQL
+SELECT colone_1, colone_2, colone_3, colone_4, colone_5, colone_6, colone_7, colone_8, colone_9, colone_10,colone_11, colone_12, colone_13, colone_14, colone_15, colone_16, colone_17, colone_18, colone_19, colone_20, colone_21, colone_22, colone_23, colone_24, colone_25,colone_26, colone_27, colone_28, colone_29, colone_30
+FROM niveau_5
+WHERE niveau_5.colone_30 = colone_30$$
+DELIMITER ;
